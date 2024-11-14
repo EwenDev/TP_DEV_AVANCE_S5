0836d10d02fc44eb4485fd258d86102d0919d990
Ewen Gilbert <wenwengilbert@gmail.com>
2024-11-14 18:00:08 +0100

    add "Exercice II" content on UneFenetre.java

--- TP1_Mobile/UneFenetre.java
+++ TP1_Mobile/UneFenetre.java
@@ -1,24 +1,42 @@
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
-class UneFenetre extends JFrame 
+class UneFenetre extends JFrame
 {
     UnMobile sonMobile;
+    Thread laTache;
     JButton sonBouton;
-    private final int LARG=400, HAUT=250;
+    private final int LARG = 400, HAUT = 250;
 
     public UneFenetre()
     {
-    super("le mobile");
-    Container leContainer = getContentPane();
-    sonMobile = new UnMobile(LARG, HAUT);
-    sonBouton = new JButton("Start/Stop");
-    sonBouton.setPreferredSize(new Dimension(10, 10));
-    leContainer.add(sonMobile);
-    leContainer.add(sonBouton);
-    Thread laTache = new Thread(sonMobile);
-    laTache.start();
-    setSize(LARG, HAUT);
-    setVisible(true);
+        sonMobile = new UnMobile(LARG, HAUT);
+        add(sonMobile, BorderLayout.CENTER);
+
+        sonBouton = new JButton("Stop");
+        add(sonBouton, BorderLayout.SOUTH);
+
+        setSize(LARG, HAUT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+
+        laTache = new Thread(sonMobile);
+        laTache.start();
+
+        sonBouton.addActionListener(new ActionListener() {
+            boolean isRunning = true;
+
+            public void actionPerformed(ActionEvent e) {
+                if (isRunning) {
+                    laTache.suspend();
+                    sonBouton.setText("Start");
+                } else {
+                    laTache.resume();
+                    sonBouton.setText("Stop");
+                }
+                isRunning = !isRunning;
+            }
+        });
     }
 }
